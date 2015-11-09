a6134e24ce62dbd5f5d8c0752216e39b4534f725
wzq <qq960826@gmail.com>
2015-11-09 12:42:04 +0800

    侧边栏进一步更新

--- app/src/main/java/com/isdust/www/baseactivity/BaseMainActivity_new.java
+++ app/src/main/java/com/isdust/www/baseactivity/BaseMainActivity_new.java
@@ -3,13 +3,11 @@ package com.isdust.www.baseactivity;
 import android.app.Activity;
 import android.content.Context;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.isdust.www.MyApplication;
 import com.isdust.www.R;
-import com.isdust.www.SlideMenu;
 import com.isdust.www.menu.Leftmenu;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
@@ -18,7 +16,6 @@ import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
  */
 public class BaseMainActivity_new extends Activity {
     protected MyApplication isdustapp;	//通过app调全局变量
-    protected SlideMenu slideMenu;    //侧边栏
     protected Context mContext;
     private long exitTime = 0;
     Leftmenu mLeftmenu;
@@ -37,17 +34,7 @@ public class BaseMainActivity_new extends Activity {
         menu = mLeftmenu.menu;
     }   //初始化
 
-    public void onTitleBarClick(View v) {
-        switch (v.getId()) {
-            case R.id.title_bar_menu_btn:
-                if (slideMenu.isMainScreenShowing()) {
-                    slideMenu.openMenu();
-                } else {
-                    slideMenu.closeMenu();
-                }
-                break;
-        }
-    }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)
     {
