64f0b5256651a0b272d1872ac2c98cbb4e0230d9
wzq <qq960826@gmail.com>
2015-11-12 12:30:16 +0800

    空自习防止攻击

--- app/src/main/java/pw/isdust/isdust/function/EmptyClassroom.java
+++ app/src/main/java/pw/isdust/isdust/function/EmptyClassroom.java
@@ -75,9 +75,9 @@ public class EmptyClassroom {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        submit=submit.replace("==","");
         //?method=4&building=&zhoushu=&xingqi=&jieci=
-        String text=mHttp.post_string("http://kzxs.isdust.com/chaxun_new.php","data="+submit);
+        String text=mHttp.post_string("http://kzxs.isdust.com/chaxun_new.php","data="+submit+"&verification="+Networklogin_CMCC.md5(submit+"dsfwedsdv"+time)+"&time="+time);
 
 //        String text=mHttp.get_string("http://kzxs.isdust.com/chaxun.php?method=4&building="+building+"&zhoushu="+ schooldate +"&xingqi="+week+"&jieci="+jieci);
         ScheduleInformation[]result=jiexi(text);
