c242bb9d9edb56777816cc984a9c61069e945877
wzq <qq960826@gmail.com>
2015-11-23 15:37:16 +0800

    加入生活服务

---
+++ app/src/main/java/com/isdust/www/LifeActivity.java
@@ -0,0 +1,38 @@
+package com.isdust.www;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.isdust.www.baseactivity.BaseMainActivity_new;
+
+/**
+ * Created by wzq on 15/11/23.
+ */
+public class LifeActivity extends BaseMainActivity_new {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        INIT(R.layout.helper_life, "生活服务",8);
+    }
+    public void onFormJiaowuClick(View v) {
+        //设置传递方向
+        Intent intent = new Intent();
+        switch (v.getId()) {
+            case R.id.life_button_dianfei:
+                intent.setClass(this,EmptyRoomActivity.class);
+                //启动activity
+                this.startActivity(intent);
+                break;
+            case R.id.life_button_map:
+                intent.setClass(this,ScheduleActivity.class);
+                //启动activity
+                this.startActivity(intent);
+                break;
+
+
+
+        }
+    }
+}
