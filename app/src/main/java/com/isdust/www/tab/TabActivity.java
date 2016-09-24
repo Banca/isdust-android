bf9215edfce8579fdfdad41634e694c9aa0c59d5
zorpan <1573344126@qq.com>
2016-09-24 10:36:21 +0800

    ui修改

---
+++ app/src/main/java/com/isdust/www/tab/TabActivity.java
@@ -0,0 +1,13 @@
+package com.isdust.www.tab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+    }
+}
